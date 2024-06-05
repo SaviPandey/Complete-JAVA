b2d2f0b0902e9cc566d8a93f7eadedb73c84b151
Savinay <21102125.savinay.pandey@gmail.com>
2024-06-05 20:47:59 +0530

    Print numbers in increasing order

--- Recursion/PrintIncrease.java
+++ Recursion/PrintIncrease.java
@@ -1,14 +1,15 @@
 public class PrintIncrease  {
     public static void printInc(int n){
-        if(n==5){
-            System.out.print(n);
+        if(n==1){
+            System.out.print(n+" ");
             return;
         }
+        printInc(n-1);
         System.out.print(n+" ");
-        printInc(n+1);
+        
     }
     public static void main(String args[]){
-        int n=1;
+        int n=10;
         printInc(n);
     }
 }
