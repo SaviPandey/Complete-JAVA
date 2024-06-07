fe350c276cc1525ad07133ba91c6f48aa08d9da4
Savinay <21102125.savinay.pandey@gmail.com>
2024-06-07 15:48:37 +0530

    Count number of ways to tile the floor

---
+++ Recursion/Recursion-Advance/TilingProblem.java
@@ -0,0 +1,23 @@
+// Amazon - Tiling Problem
+
+public class TilingProblem {
+    public static int tilingProb(int n){ //2 x n (florr size)
+        if(n==0 || n==1){
+            return 1;       //only 1 way to put tile
+        }
+        // vertical tile place
+        int fnm1 = tilingProb(n-1);
+
+        // horizontal tile place
+        int fnm2 = tilingProb(n-2);
+
+        int tol = fnm1 + fnm2;
+        return tol;
+
+    }
+    public static void main(String args[]){
+        int n =4;
+        System.out.println(tilingProb(n));
+    }                                         
+    
+}
