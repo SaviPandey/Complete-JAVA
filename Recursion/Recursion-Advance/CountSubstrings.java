853a42d71c76fc24304aca578c706339fe979112
Savinay <21102125.savinay.pandey@gmail.com>
2024-06-10 14:03:03 +0530

    Count number of SubStrings

--- Recursion/Recursion-Advance/CountSubstrings.java
+++ Recursion/Recursion-Advance/CountSubstrings.java
@@ -8,7 +8,10 @@ public class CountSubstrings{
         if(n <= 0){
             return 0;
         }
-        int res = countSubstrs(str , i+1 , j , n-1) + countSubstrs(str ,i, j-1, n-1) - countSubstrs(str , i+1, j-1 , n-2) ;
+        int res = countSubstrs(str , i+1 , j , n-1) + 
+                    countSubstrs(str ,i, j-1, n-1) - 
+                    countSubstrs(str , i+1, j-1 , n-2) ;
+                    
         if(str.charAt(i) == str.charAt(j)){
             res++;
         }
