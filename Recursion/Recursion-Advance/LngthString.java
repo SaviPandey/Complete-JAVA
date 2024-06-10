1b6be2bb71483d3a4385016457e3db48678171ba
Savinay <21102125.savinay.pandey@gmail.com>
2024-06-10 14:00:17 +0530

    Find Length of String using Recursion

---
+++ Recursion/Recursion-Advance/LngthString.java
@@ -0,0 +1,14 @@
+//Recursion-3
+public class LngthString {
+    public static int length(String str){
+        if(str.length() == 0){
+            return 0;
+        }
+
+        return length(str.substring(1)) + 1;
+    }
+    public static void main(String args[]){
+        String str = "abcde";
+        System.out.println(length(str));
+    }
+}
