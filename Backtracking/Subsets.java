65dcc6b5f1bd5f3bff9bf1b61afc8d4dc6048fcb
Savinay <21102125.savinay.pandey@gmail.com>
2024-06-12 14:42:17 +0530

    Print all subsets of a String

---
+++ Backtracking/Subsets.java
@@ -0,0 +1,25 @@
+public class Subsets {
+    public static void findSubsets(String str , String ans, int i){
+        //base case
+        if(i == str.length()){
+            if(ans.length() == 0){
+                System.out.print("Null");
+            }else{
+            System.out.print(ans+" ->");
+            }
+            return;
+        }
+        //Recursion
+        //Yes Choice
+        findSubsets(str, ans+str.charAt(i), i+1);
+        //No choice
+        findSubsets(str, ans, i+1);
+
+
+    }
+    public static void main(String args[]){
+        String str= "abc";
+        String ans ="";
+        findSubsets(str, ans, 0);
+    }
+}
