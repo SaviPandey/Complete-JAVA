fa720da6d35522f9cac4e4bcaddc705cabbabc4d
Savinay <21102125.savinay.pandey@gmail.com>
2024-06-14 16:08:29 +0530

    Find Max Value using ArrayList

---
+++ ArrayList/FindMaxNum.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+public class FindMaxNum {
+    public static void main(String args[]){
+        int maxNum = Integer.MIN_VALUE;
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(89);
+        list.add(7);
+        list.add(9);
+        list.add(25);
+        list.add(24);
+
+        for(int i=0; i<list.size(); i++){
+            if(list.get(i) > maxNum )
+                maxNum = list.get(i);
+        }
+        System.out.println("Maximum : "+maxNum);
+        
+    }
+}
