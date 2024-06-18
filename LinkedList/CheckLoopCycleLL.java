79c1fc88d1f4cce414bb40f1b55b632e49cde620
Savinay Pandey <21102125.savinay.pandey@gmail.com>
2024-06-18 18:02:04 +0530

    Detect Loop Cycle in LL

---
+++ LinkedList/CheckLoopCycleLL.java
@@ -0,0 +1,60 @@
+public class CheckLoopCycleLL {
+    public static class Node {
+        int data ;
+        Node next ;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+    public void printLL(){
+        if(size == 0){
+            System.out.println("Empty");
+            return;
+        } 
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data+"->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+    public boolean detectCycle(){
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true; //cycle exists
+            } 
+        }
+        return false;
+    }
+
+    public static void main(String args[]){
+        CheckLoopCycleLL c = new CheckLoopCycleLL();
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = head;
+
+        System.out.println(c.detectCycle());
+    }    
+}
