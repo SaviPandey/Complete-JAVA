37c992434e355c1014074051f8aca401907ce24d
Savinay Pandey <21102125.savinay.pandey@gmail.com>
2024-06-19 11:30:19 +0530

    Updated RemoveCycle

--- LinkedList/RemoveCycle.java
+++ LinkedList/RemoveCycle.java
@@ -29,7 +29,7 @@ public class RemoveCycle {
         Node slow = head;
         Node fast = head;
         boolean cycle = false;
-        while(fast != null || fast.next != null){
+        while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
             if(slow == fast){
