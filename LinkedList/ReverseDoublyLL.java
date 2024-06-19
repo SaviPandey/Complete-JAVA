99cd4cb65881510b6fb0fab046cd83c95c68299e
Savinay Pandey <21102125.savinay.pandey@gmail.com>
2024-06-19 17:41:48 +0530

    Reverse a Doubly-LL

---
+++ LinkedList/ReverseDoublyLL.java
@@ -0,0 +1,67 @@
+public class ReverseDoublyLL {
+    public static class Node{
+        int data;
+        Node next;
+        Node prev;
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        size++;
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+    public void printLL(){
+        if(head == null){
+            System.out.println("List is empty");
+            return;
+        }
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data+" <-> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void reverse(){
+        Node prev = null;
+        Node curr = tail = head;
+        Node next;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            curr.prev = next;
+
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+    public static void main(String args[]){
+        ReverseDoublyLL r = new ReverseDoublyLL();
+        r.addFirst(1);
+        r.addFirst(2);
+        r.addFirst(3);
+        r.addFirst(4);
+        r.addFirst(5);
+
+        r.printLL();
+        r.reverse();
+        r.printLL();
+
+    }
+}
