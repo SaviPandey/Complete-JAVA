5e74d365494de60684af3287ca9aca487106dcfb
Savinay Pandey <21102125.savinay.pandey@gmail.com>
2024-06-21 12:54:21 +0530

    Stack using Linked Lists

---
+++ Stack/StackLL.java
@@ -0,0 +1,54 @@
+public class StackLL {
+    static class Node {
+        int data ;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    static class Stack {
+        static Node head = null;
+
+        public boolean isEmpty(){
+            return head == null;
+        }
+
+        public void push(int data) {
+            Node newNode = new Node(data);
+            if(head == null) {
+                head = newNode;
+                return;
+            }
+            newNode.next = head;
+            head = newNode;
+        }
+
+        public int pop() {
+            if(isEmpty()) {
+                return -1;
+            }
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        public int peek() {
+            if(isEmpty()) {
+                return -1;
+            }
+            return head.data;
+        }
+    }
+    public static void main(String args[]){
+        Stack s = new Stack();
+        s.push(1);
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while(!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
+    }
+}
