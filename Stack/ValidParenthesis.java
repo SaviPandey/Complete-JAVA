046ad9692a14ac84a0dc92ea50da8939b39edca0
Savinay Pandey <21102125.savinay.pandey@gmail.com>
2024-06-21 17:39:57 +0530

    Check Given string for Valid Parenthesis

---
+++ Stack/ValidParenthesis.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class ValidParenthesis {
+    public static boolean isValid(String str) {
+        Stack<Character> s = new Stack<>();
+        for(int i=0; i<str.length(); i++) {
+            char c = str.charAt(i);
+            //Opening
+            if(c == '(' || c =='{' ||c == '[') {
+                s.push(c);
+            }
+            //Closing 
+            else {
+                if(s.isEmpty()) return false;
+
+                if(s.peek() == '{' && c == '}' || s.peek() == '(' && c == ')' || s.peek() == '[' && c == ']') {
+                    s.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return s.isEmpty();   //At end if stack is empty then true, else false
+    }
+    public static void main(String[] args) {
+        String str = "([]{})";
+        System.out.println(isValid(str));
+    }
+}
