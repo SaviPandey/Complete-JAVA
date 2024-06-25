c6cdadae2162b22f392448c68d9a564a830087fe
Savinay Pandey <21102125.savinay.pandey@gmail.com>
2024-06-25 15:30:37 +0530

    PreOrder Traversal - Time [O(n)]

--- BinaryTrees/BinaryTreeB.java
+++ BinaryTrees/BinaryTreeB.java
@@ -23,11 +23,21 @@ public class BinaryTreeB {
             return newNode;
         }
     }
+    public static void preorderTraversal(Node root){
+        if(root == null) { //For emmpty tree or Leaf node
+            return;
+        }
+        System.out.print(root.data+" ");
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+    }
 
     public static void main(String[] args) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         System.out.print("Root Data: "+root.data);
+        System.out.println("\nPreorder Traversal: ");
+        preorderTraversal(root);
     }
 }
