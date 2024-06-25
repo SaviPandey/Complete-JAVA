6d3f17aafa7431ca6969933b3025d97307989b0c
Savinay Pandey <21102125.savinay.pandey@gmail.com>
2024-06-25 15:11:01 +0530

    Build Tree from the PreOrder Sequence

---
+++ BinaryTrees/BinaryTreeB.java
@@ -0,0 +1,33 @@
+public class BinaryTreeB {
+    static class Node{
+        int data;
+        Node left, right;
+
+        Node(int data){
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+    static class BinaryTree {
+        static int idx = -1;
+        public Node buildTree(int nodes[]) {
+            idx++;
+            if(nodes[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        System.out.print("Root Data: "+root.data);
+    }
+}
