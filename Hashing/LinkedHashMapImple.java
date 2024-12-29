78866ec1707b5994b0e675030cc1df61760da083
Savinay Pandey <21102125.savinay.pandey@gmail.com>
2024-12-29 14:10:59 +0530

    LinkedHashMapImplementation

---
+++ Hashing/LinkedHashMapImple.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class LinkedHashMapImple {
+    public static void main(String args[]){
+        //Order of Insertion is saved because Doubly LinkedList is used , rest all the operations are same
+        LinkedHashMap<String, Integer> lhm = new LinkedHashMap<>();
+        lhm.put("India", 200);
+        lhm.put("USA", 300);
+        lhm.put("China", 150);
+
+        System.out.println(lhm);
+    }
+
+
+}
