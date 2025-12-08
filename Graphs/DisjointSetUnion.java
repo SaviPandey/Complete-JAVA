35ba090706cc44843bab7589e0047359e131a0e3
Savinay Pandey <21102125.savinay.pandey@gmail.com>
2025-12-09 05:08:40 +0530

    New DataStructure DisjointSet(Union-find) with it's two function union() & find

---
+++ Graphs/DisjointSetUnion.java
@@ -0,0 +1,56 @@
+package Graphs;
+
+public class DisjointSetUnion {
+    static int n = 7;
+    static int par[] = new int[n];
+    static int rank[] = new int[n];
+
+    public static void init() {
+        for (int i = 0; i < n; i++) {
+            par[i] = i; // Initially all nodes are parents of themselves
+        }
+    }
+
+    public static int find(int x) {
+        if (x == par[x])
+            return x;
+
+        return find(par[x]);
+    }
+
+    // PATH-COMPRESSION ON find()
+    public static int findPathCompressed(int x) {
+        if (x == par[x])
+            return x;
+
+        return par[x] = find(par[x]);
+    }
+
+    public static void union(int a, int b) {
+        int parA = find(a);
+        int parB = find(b);
+
+        if (rank[parA] == rank[parB]) {
+            par[parB] = parA;
+            rank[parA]++;
+        } else if (rank[parA] < rank[parB]) {
+            par[parA] = parB;
+        } else {
+            par[parB] = parA;
+        }
+    }
+
+    public static void main(String[] args) {
+        init();
+
+        // List of Operations
+        System.out.println(find(3));
+        union(1, 3);
+        union(2, 4);
+        union(3, 6);
+        union(1, 4);
+        System.out.println(find(3));
+        System.out.println(find(4));
+        union(1, 5);
+    }
+}
